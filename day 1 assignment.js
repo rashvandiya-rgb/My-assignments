3f8a6cb50eda5ed93a0a3d4d514147ba843b881e
rashvandiya-rgb <rashvandiya@gmail.com>
2025-09-21 13:11:31 +0530

    Add files via upload
    My day 1 assignment

---
+++ day 1 assignment.js
@@ -0,0 +1,12 @@
+//Day 1 homework
+var firstname='Rashu';
+console.log(firstname);
+console.log(typeof firstname);
+
+var company_name='Syncfusion';
+console.log(company_name);
+console.log(typeof company_name);
+
+var mobile_number= 9876543210;
+console.log(mobile_number);
+console.log(typeof mobile_number)
